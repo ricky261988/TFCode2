77fe2a1fac7c3a99eec0d223ca484876845f6686
Kishan Chaurasia <kishan.chaurasiya@gmail.com>
2024-11-29 09:20:31 +0000

    This is my first TF Code

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "prefix" {
+
+}
+
+variable "location" {
+
+}
+
+variable "vm_size" {
+
+}
+
+variable "computer_name" {
+
+}
+
+variable "admin_username" {
+
+}
+
+variable "admin_password" {
+
+}
